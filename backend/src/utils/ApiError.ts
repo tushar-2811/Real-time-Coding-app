eb76337c6dc91cbcb5ecb6b64fc6bd67892ece24
Tushar Rawat <tusharrawat52@gmail.com>
2024-06-15 23:25:35 +0530

    add ApiError for error response

---
+++ backend/src/utils/ApiError.ts
@@ -0,0 +1,29 @@
+class ApiError extends Error {
+    statusCode: number;
+    errors : any[];
+    data : any;
+    success : boolean;
+    
+    constructor(
+       statusCode: number,
+       message: string = "Something went wrong",
+       errors: any[] = [],
+       stack: string = "",
+    ) {
+         super(message);
+         this.statusCode = statusCode;
+         this.data = null;
+         this.message = message;
+         this.success = false;
+         this.errors = errors;
+
+
+         if(stack) {
+           this.stack = stack;
+         }else{
+           Error.captureStackTrace(this , this.constructor)
+         }
+    }
+}
+
+export {ApiError};
