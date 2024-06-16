e45f80b7ab0a68cf705941ef04be4e609079b7a3
Tushar Rawat <tusharrawat52@gmail.com>
2024-06-16 23:09:04 +0530

    add user controllers

---
+++ backend/src/controllers/api/v1/user/userController.ts
@@ -0,0 +1,14 @@
+import { asyncHandler } from "../../../../utils/asyncHandler";
+import { Request , Response } from "express";
+
+
+// sign-up controller
+export const signUpController = asyncHandler(async (req: Request , res: Response) => {
+       
+})
+
+
+// sign-in controller
+export const signInController = asyncHandler(async (req: Request , res: Response) => {
+       
+})
--- backend/src/routes/api/v1/user/userRouter.ts
+++ backend/src/routes/api/v1/user/userRouter.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { signInController, signUpController } from "../../../../controllers/api/v1/user/userController";
 
 const userRouter = Router();
 
-userRouter.route('/sign-up').post()
+userRouter.route('/sign-up').post(signUpController);
+userRouter.route('/sign-in').post(signInController);
 
 export default userRouter;
