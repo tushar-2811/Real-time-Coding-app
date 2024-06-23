426dcc18fa0a60d5c03ca5a369154426f7857deb
Tushar Rawat <tusharrawat52@gmail.com>
2024-06-23 19:13:33 +0530

    add get Problem controller

--- backend/src/controllers/api/v1/problems/problemController.ts
+++ backend/src/controllers/api/v1/problems/problemController.ts
@@ -2,17 +2,22 @@ import { Request , Response } from "express";
 import { AsyncHandler } from "../../../../utils/AsyncHandler";
 
 
-// add problem controller
+// add new problem
 export const addProblemController = AsyncHandler(async(req: Request , res: Response) => {
 
 });
 
-// remove problem controller
+// delete problem by Id
 export const removeProblemController = AsyncHandler((async(req: Request , res: Response) => {
 
 }));
 
-// show all problems controller
+// get all problems 
 export const getAllProblemController = AsyncHandler(async(req: Request , res: Response) => {
     
+})
+
+// get particular problem by Id
+export const getProblemWithIdController = AsyncHandler(async(req: Request , res: Response) => {
+
 })
